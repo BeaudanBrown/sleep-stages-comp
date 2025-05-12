5dafcac3ef1274404083a0b5e01fbb6391942c4c
Beaudan Brown <beaudan.brown@gmail.com>
2025-05-12 15:19:40 +1000

    Update to new flake

--- flake.nix
+++ flake.nix
@@ -1,19 +1,47 @@
 {
-  description = "A basic flake with a shell";
-  outputs = { nixpkgs, flake-utils, ... }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgsUnstable,
+      flake-utils,
+      pre-commit-hooks,
+      ...
+    }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        pkgsUnstable = nixpkgsUnstable.legacyPackages.${system};
       in
-        {
+      {
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              air-fmt = {
+                enable = true;
+                entry = "air format";
+                files = ".*\.[rR]$";
+              };
+            };
+          };
+        };
         devShells.default = pkgs.mkShell {
-          env.R_LIBS_USER="./.Rlib";
-          packages = with pkgs;
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+          env.R_LIBS_USER = "./.Rlib";
+          buildInpus = [
+            pkgs.bashInteractive
+            self.checks.${system}.pre-commit-check.enabledPackages
+          ];
+          packages =
+            with pkgs;
             [
               R
               quarto
-            ] ++ (with rPackages; [
+            ] ++ (with pkgsUnstable; [
+              air-formatter
+            ]) ++ (with rPackages; [
               languageserver
               dotenv
               data_table
@@ -28,7 +56,9 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgsUnstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
     flake-utils = {
       url = "github:numtide/flake-utils";
       inputs.systems.follows = "systems";
